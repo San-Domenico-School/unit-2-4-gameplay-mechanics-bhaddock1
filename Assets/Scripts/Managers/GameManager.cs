fd6ebe70454a7038277abf6a787f99929320a636
bhaddock1 <bhaddock25@sandomenico.org>
2024-01-10 15:10:18 -0800

    mid 4.2

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Player Fields")]
+    public Vector3 playerScale;
+    public float playerMass, playerDrag, playerMoveForce, playerRepelForce, playerBounce;
+
+    [Header("Scene Fields")]
+    public GameObject[] wayPoints;
+
+    [Header("Debug Fields")]
+    public bool debugSpawnWolves, debugSpawnPortal, debugSpawnPowerUp, debugPowerUpRepel;
+
+
     // Start is called before the first frame update
     void Start()
     {
